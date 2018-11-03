0bd015b3e9737eb5ddf418e035f8781aaaae086b
Daniel Andrews <danielandrewsre@gmail.com>
2018-11-03 15:19:10 -0700

    added pod for notification banner

--- Pods/Target Support Files/Pods-Five/Pods-Five-frameworks.sh
+++ Pods/Target Support Files/Pods-Five/Pods-Five-frameworks.sh
@@ -143,10 +143,12 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/BRYXBanner/BRYXBanner.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/GradientView/GradientView.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/UIColor_Hex_Swift/UIColor_Hex_Swift.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/BRYXBanner/BRYXBanner.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/GradientView/GradientView.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/UIColor_Hex_Swift/UIColor_Hex_Swift.framework"
 fi
